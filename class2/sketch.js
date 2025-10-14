9d21f5a06afdcf2d8ac91d77d95bce7461f83a20
Enea-t <enea.tramontana@mail.polimi.it>
2025-10-14 11:43:15 +0200

    Class4 - inclass dev

--- class2/sketch.js
+++ class2/sketch.js
@@ -4,12 +4,68 @@ let yMax = 600;
 let xRocket = xMax/2;
 let yRocket = yMax*0.6
 
+let table;
+let star_img;
+let rocket_img
+
+function preload() {
+  table = loadTable("../Stelle/stars.csv", "csv", "header")
+  star_img = loadImage("../Stelle/star.png")
+}
+
 function setup() {
   createCanvas(xMax, yMax);
+  frameRate(10);
+}
+
+function drawSingleStarFromFile(index, posX, posY) {
+  let starSize = table.getNum(index, "starSize")
+  image(star_img, posX, posY, starSize, starSize)
+}
+
+function drawStarsFromFile() {
+  for(let k = 0; k < table,getRowCount(); k++) {
+    let starX = (k*37) % width + (k%3) * 5;
+    let starY = (k*73) % height + (k%7);
+
+    drawSingleStarFromFile(k, starX, starY)
+  }
+}
+
+function drawSingleStar(i, starX, starY, random_transparency, random_size) {
+  if( i % 2 == 0 ){
+      //stella tipo a
+      fill(255, 255, 150, random_transparency)
+      ellipse(starX,starY, random_size); //1
+      //stelle tipo b
+      //ci saranno
+      //per ogni i che è divisibile per 3
+    } else if ( i % 3 == 0 ){
+      //stella tipo b
+      fill(200, 100, 255, random_transparency)
+      ellipse(starX, starY, random_size) //1.5
+    } else {
+      //stella tipo c
+      fill(255, 255, 100, random_transparency)
+      ellipse(starX, starY, random_size) //2.8
+    }
+    return;
+}
+
+function drawStars(num_stars=120) {
+  for(let i=0; i < num_stars; i++){
+    let starX = (i*37) % width + (i%3)*5;
+    let starY = (i*73) % height + (i%7)
+
+    let random_transparency = random(150, 255)
+    let random_size = random(5, 7)
+
+    drawSingleStar(i, starX, starY, random_transparency, random_size)
+  }
 }
 
 function draw() {
-  background("#c0e1fc");
+  background("#06243dff");
   //mostrare un testo bianco che
   //dice le coordinate del mouse
   //sul foglio da disegno
@@ -27,28 +83,45 @@ function draw() {
   noStroke();
   //un unico ciclo
   //creare una sequenza x fare a, b, c
+  /*
   for(let i=0; i<120; i++){
     let starX = (i*37) % width + (i%3)*5;
     let starY = ((i*73) % height) + (i%7);
     //operatore modulo %
+
+    random_transparency = random(150, 255)
+    random_size = random(1, 2.8)
+
+
+    //Per disegnar le stelle posso scrivere tutto questo codice
+
     if( i % 2 == 0 ){
       //stella tipo a
-      fill(255, 255, 150)
-      ellipse(starX,starY, 1); //1
+      fill(255, 255, 150, random_transparency)
+      ellipse(starX,starY, random_size); //1
       //stelle tipo b
       //ci saranno
       //per ogni i che è divisibile per 3
     } else if ( i % 3 == 0 ){
       //stella tipo b
-      fill(200, 100, 255)
-      ellipse(starX, starY, 1.5) //1.5
+      fill(200, 100, 255, random_transparency)
+      ellipse(starX, starY, random_size) //1.5
     } else {
       //stella tipo c
-      fill(255, 255, 100)
-      ellipse(starX, starY, 2.8) //2.8
+      fill(255, 255, 100, random_transparency)
+      ellipse(starX, starY, random_size) //2.8
     }
-  }
+}
+*/
+    //Oppure posso chiamare la funzione drawStar che ho dichiarato prima
+    //e fa esattamente la stessa cosa
+
+    drawStar(1, starX, starY, random_transparency, random_size);
+
+  
   pop();
+
+  drawStarsFromFile()
 
   //aprire contesto di disegno
   push()
@@ -82,6 +155,8 @@ function draw() {
   //finire contesto di disegno
   pop();
 
+
+  drawRocket(xRocket, yRocket)
   //xRocket = (xRocket + 1) % xMax;
   yRocket = yRocket - 3;
   //quando la yRocket sarà oltre una certa soglia
@@ -90,4 +165,14 @@ function draw() {
     yRocket = yMax;
   }
   //allora resettare la soglia
+  yRocket = moveRocket(yRocket, 1)
+}
+
+function moveRocket(xRocket, yRocket, step=1) {
+  yRocket = yRocket - step;
+  let soglia = -(yMax*0.5)
+  if (yRocket < soglia) {
+    yRocket = yMax
+  }
+  return yRocket
 }
---
+++ class2/sketch2.js
@@ -0,0 +1,207 @@
+let xMax = 400;
+let yMax = 600;
+
+let xRocket = xMax/2;
+let yRocket = yMax*0.6;
+
+let table;
+let star_img;
+
+function preload() {
+  table = loadTable("../Stelle/stars.csv", "csv", "header");
+  star_img = loadImage("../Stelle/star.png");
+}
+
+function setup() {
+  createCanvas(xMax, yMax);
+}
+
+function drawSingleStarFromFile(index, posX, posY){
+  let starSize = table.getNum(index, "starSize")
+  image(star_img, posX, posY, starSize, starSize);
+}
+
+function drawStarsFromFile() {
+  for(let k = 0; k < table.getRowCount(); k++) {
+    let starX = (k*37) % width + (k%3) * 5;
+    let starY = (k*73) % height + (k%7);
+
+    drawSingleStarFromFile(k, starX, starY);
+  }
+}
+
+function drawSingleStar(i, starX, starY, random_transparecy, random_size) {
+  if(i % 2 == 0){ //--> tutti i numeri pari divisi x 2 danno come resto 0
+      // stella tipo a
+      fill(255,255,150,random_transparecy);
+      ellipse(starX,starY,random_size);
+      //le stelle b quando i è divisibile x 3
+    } else if(i % 3 == 0) { //alla seconda iterazione i == 2
+      //stella tipo b
+      fill(200,100,255);
+      ellipse(starX,starY,1.5);
+    } else{
+      //stella tipo c
+      fill(255,255,100);
+      ellipse(starX,starY,2.8);
+    }
+}
+
+function drawStars(num_stars = 120) {
+  for(let i=0; i < num_stars; i++){
+    let starX = (i*37) % width + (i%3) * 5;
+    let starY = (i*73) % height + (i%7);
+
+    let random_transparecy = random (150,255);
+    let random_size = random (6,10);
+
+    drawSingleStar(i, starX, starY, random_transparecy, random_size)
+  }
+
+}
+
+function drawRocketFromFile(xRocket, yRocket) {
+  image(rocket_img, xRocket, yRocket);
+}
+
+function drawRocket(xRocket, yRocket) {
+  push();
+  fill(220);
+  stroke(40);
+
+  rectMode(CENTER); // il rettangolo parte dal centro
+  rect(xRocket,yRocket+30,80,180,20);
+  //
+  //pop();
+  
+  //triangle
+  fill(200,40,40);
+  strokeWeight(2);
+  triangle(xRocket-40,yRocket-60,xRocket+40,yRocket-60,xRocket,yRocket-120);
+
+  //circle
+  fill(40,150,220);
+  stroke(255);
+  strokeWeight(3);
+  ellipse(xRocket,yRocket+30,48,48);
+
+  //triangolini
+  fill(200,40,40);
+  stroke(40);
+  strokeWeight(2)
+  triangle(xRocket-40,yRocket+90,xRocket-20,yRocket+90,xRocket-70,yRocket+120);
+
+  triangle(xRocket+40,yRocket+90,xRocket+20,yRocket+90,xRocket+70,yRocket+120);
+  pop();
+}
+
+
+function moveRocket(yRocket, step=1){
+  yRocket = yRocket - step;
+  let soglia = -(yMax * 0.6);
+  if (yRocket < soglia) {
+    yRocket = yMax;
+  }
+  return yRocket;
+}
+
+
+
+function draw() {
+  background(200, 200, 200);
+  // mostrare un testo bianco
+  // che dice le coordinate al mouse
+  //sul foglio da disegno
+  fill(255); //bianco
+  textSize(20);
+  text("mouseX: " + mouseX +  ",    mouseY: " + mouseY,20,20);  //stringa, x, y
+
+
+  /*
+  // disegnare le stelle
+  // 120
+  // tre tipi di stelle: a, b, c
+  // fino a che abbiamo 120 stelle
+  // stelle ellipse
+  push();
+
+  // un unico ciclo
+  // creare una sequenza per fare a, b, c --> selezione (condizionale)
+  noStroke(); //toglie lo stroke a tutti gli elementi di questo contesto
+
+  for(let i=0; i < 120; i++){
+    let starX = ((i*37) % width) + (i%3) * 5;
+    let starY = ((i*73) % height) + (i%7);
+
+    let random_x = random(0,width);
+    let random_y = random(0,height);
+
+    random_transparecy = random(150,255);
+    random_size = random(2.8,5.0);
+    // operatore modulo %
+    //stella a quando i è pari
+   
+    drawStar(i, starX, starY, random_transparecy, random_size);
+  }
+  //finisce il contesto
+  pop();
+*/
+
+//drawStars(100);
+
+
+  /*
+  // nuovo contesto di disegno
+  push();
+  fill(220);
+  stroke(40);
+
+  rectMode(CENTER); // il rettangolo parte dal centro
+  rect(xRocket,yRocket+30,80,180,20);
+  //
+  //pop();
+  
+  //triangle
+  fill(200,40,40);
+  strokeWeight(2);
+  triangle(xRocket-40,yRocket-60,xRocket+40,yRocket-60,xRocket,yRocket-120);
+
+  //circle
+  fill(40,150,220);
+  stroke(255);
+  strokeWeight(3);
+  ellipse(xRocket,yRocket+30,48,48);
+
+  //triangolini
+  fill(200,40,40);
+  stroke(40);
+  strokeWeight(2)
+  triangle(xRocket-40,yRocket+90,xRocket-20,yRocket+90,xRocket-70,yRocket+120);
+
+  triangle(xRocket+40,yRocket+90,xRocket+20,yRocket+90,xRocket+70,yRocket+120)
+
+  pop();
+  */
+drawStarsFromFile();
+pop();
+
+//drawRocket(xRocket, yRocket);
+drawRocketFromFile(xRocket, yRocket, 50, 50);
+
+yRocket = moveRocket(yRocket, step=1);
+
+  /*
+ // xRocket = (xRocket+1) % xMax;
+  yRocket = (yRocket-1);
+  //quando la yRocket sarà oltre una certa soglia
+  // allora dobbiamo resettarla
+  let soglia = -(yMax * 0.6);
+  if(yRocket < soglia) {
+    yRocket = yMax;
+  }
+    */
+
+
+
+
+}
